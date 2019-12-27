61ce399444be9357b3b10afedda539a1d01e0745
EricElmoznino <eric.elmoznino@gmail.com>
2019-12-27 03:07:41 -0500

    Debugging deep dream algorithm

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ layers = list(model.features.children())
 model = torch.nn.Sequential(*layers[: (27 + 1)])
 loss_func = roi_loss_func(towards_target=False)
 
-img = image_to_tensor('/home/eelmozn1/Downloads/dog.jpg')
+img = image_to_tensor('/home/eelmozn1/Downloads/sky1024px.jpg')
 
 pert = deepdream(img, model, torch.zeros(1000), loss_func,
                  n_octave=10, octave_scale=1.4, alpha=0.01, n_iter=20, max_jitter=0)

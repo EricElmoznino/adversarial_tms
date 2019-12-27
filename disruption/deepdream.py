7727cd16de320a23033aff9dc9ae77800217a4d4
EricElmoznino <eric.elmoznino@gmail.com>
2019-12-27 01:57:40 -0500

    Debugging deep dream algorithm

--- disruption/deepdream.py
+++ disruption/deepdream.py
@@ -2,6 +2,7 @@ import torch
 from torch.nn import functional as F
 import numpy as np
 import utils
+import scipy.ndimage as nd
 
 
 def deepdream(orig, model, target, loss_func,
@@ -17,15 +18,18 @@ def deepdream(orig, model, target, loss_func,
     octaves = [orig]
     orig_size = np.array(orig.shape[-2:])
     for n in range(1, n_octave):
-        size = (orig_size * (octave_scale ** -n)).astype(np.int).tolist()
-        octaves.append(F.interpolate(orig, size=size, mode='bilinear'))
+        img = octaves[-1].cpu().numpy()
+        img = nd.zoom(img, (1, 1, 1 / octave_scale, 1 / octave_scale), order=1)
+        img = torch.from_numpy(img).to(target.device)
 
     detail = None
     for octave_base in octaves[::-1]:
         if detail is None:
             detail = torch.zeros_like(octave_base)
         else:
-            detail = F.interpolate(detail, size=octave_base.shape[-2:], mode='bilinear')
+            img = detail.cpu().numpy()
+            img = nd.zoom(img, np.array(octave_base.shape) / np.array(detail.shape), order=1)
+            detail = torch.from_numpy(img).to(target.device)
 
         input_octave = octave_base + detail
         output = dream(input_octave, model, target, loss_func,

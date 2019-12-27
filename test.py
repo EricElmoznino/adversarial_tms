52c040d4d76ee7573f9f66f712076c019d4edb53
EricElmoznino <eric.elmoznino@gmail.com>
2019-12-27 03:08:48 -0500

    Debugging deep dream algorithm

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ loss_func = roi_loss_func(towards_target=False)
 img = image_to_tensor('/home/eelmozn1/Downloads/sky1024px.jpg')
 
 pert = deepdream(img, model, torch.zeros(1000), loss_func,
-                 n_octave=10, octave_scale=1.4, alpha=0.01, n_iter=20, max_jitter=0)
+                 n_octave=4, octave_scale=1.4, alpha=0.01)
 
 pert = tensor_to_image(pert)
 pert.save('/home/eelmozn1/Downloads/output.jpg')

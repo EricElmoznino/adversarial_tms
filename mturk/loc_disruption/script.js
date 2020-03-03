7c9e1bf4a95ee748e84aa2891d2fd9078186d96a
EricElmoznino <eric.elmoznino@gmail.com>
2020-03-03 17:47:07 -0500

    Modifications to the loc disruption mturk task so remove the possibility of any bugs, and also to give fixed foil categories for each image

--- mturk/loc_disruption/script.js
+++ mturk/loc_disruption/script.js
@@ -9,14 +9,16 @@ let rootPath = "";
 /* Globals */
 var trials = [];
 var curTrial = 0;
+var curResponse = null;
 var nTraining;
 var responseOptionValues;
 var trialStartTime;
 var training = true;
 var canProceed = true;
+var stimPresenting = false;
 
 /* Responses */
-var trialResponses = [];
+var responses = [];
 var displayedImages = [];
 var types = [];
 var trueCats = [];
@@ -24,246 +26,269 @@ var foilCats = [];
 var reactionTimes = [];
 
 function trialDone() {
-  var trialEndTime = new Date();
-  var rt = trialEndTime - trialStartTime;
-  if (!training) {
-    reactionTimes.push(rt);
-  }
+    if (!training) {
+        // Record the response
+        responses.push(curResponse);
 
-  curTrial++;
+        // Record what stimuli were displayed
+        trial = trials[curTrial];
+        displayedImages.push(trial["image"]);
+        types.push(trial["type"]);
+        trueCats.push(trial["trueCat"]);
+        foilCats.push(trial["foilCat"]);
 
-  // Finished experiment
-  if (curTrial >= nTrials) {
-    exportData();
-    $('#trial').hide();
-    $('#submitButton').show();
-    return;
-  }
-
-  // Check if training is done
-  if (curTrial == nTraining) {
-    finishedTraining();
-  }
-
-  // Wait for the signal to continue to the next trial
-  $('#nextTrialMessage').show();
-  $(document).bind("keydown.nextTrial", function(event) {
-    if (event.which == 32 && canProceed) {
-      $(document).unbind("keydown.nextTrial");
-      $('#trialOptions').hide();
-      $('#nextTrialMessage').hide();
-      $('#option1box').css("background-color", "white");
-      $('#option2box').css("background-color", "white");
-      if (curTrial == nTraining) {
-        $('#sessionMode').html("Experiment segment")
-      }
-      trialBegin(curTrial);
-      trialStartTime = new Date();
+        var trialEndTime = new Date();
+        var rt = trialEndTime - trialStartTime;
+        reactionTimes.push(rt);
     }
-  });
+
+    if (curTrial === nTraining - 1) {
+        training = false;
+    }
+
+    curTrial++;
+
+    // Finished experiment
+    if (curTrial >= trials.length) {
+        doneExperiment();
+        return;
+    }
+
+    curResponse = null;
+    trialBegin();
 }
 
-function trialBegin(trialNum) {
-  // Prepare the stimulus data
-  loadTrialData(trialNum);
+function trialBegin() {
+    trialStartTime = new Date();
 
-  // Pick presentation time based on if this is a catch trial or not
-  var presentationTime = stimPresentationTime;
-  if (trials[trialNum]["type"] == "catch") {
-    presentationTime = catchPresentationTime;
-  }
+    // Prepare the stimulus data
+    loadTrialData();
 
-  // Present stimulus
-  $('#fixation').show();                      // Show fixation cross
-  setTimeout(function() {             // Wait briefly before stimulus presentation
-    $('#fixation').hide();
-    $('#trialImage').show();                  // Show stimulus
-    setTimeout(function() {           // Wait briefly before hiding stimulus
-      $('#trialImage').hide();                // Hide stimulus
-      setTimeout(function() {         // Wait briefly before presenting response options
-        $('#trialOptions').show();            // Present response options
-      }, 100);
-    }, presentationTime);
+    // Pick presentation time based on if this is a catch trial or not
+    var presentationTime = stimPresentationTime;
+    if (trials[curTrial]["type"] == "catch") {
+        presentationTime = catchPresentationTime;
+    }
 
-    // Wait for response
-    $(document).bind("keydown.response", function(event) {
-      if (event.which == 68) {
-        $(document).unbind("keydown.response");
-        $('#option1box').css("background-color", "lightgrey");
-        if (!training) {
-          trialResponses.push(responseOptionValues[0]);
-        }
-        trialDone();
-      }
-      if (event.which == 75) {
-        $(document).unbind("keydown.response");
-        $('#option2box').css("background-color", "lightgrey");
-        if (!training) {
-          trialResponses.push(responseOptionValues[1]);
-        }
-        trialDone();
-      }
-    });
-  }, 1000);
+    // Present stimulus
+    stimPresenting = true;
+    $('#fixation').show();                      // Show fixation cross
+    setTimeout(function () {            // Wait briefly before stimulus presentation
+        $('#fixation').hide();                  // Hide fixation cross
+        $('#trialImage').show();                // Show stimulus
+        setTimeout(function () {        // Wait briefly before hiding stimulus
+            $('#trialImage').hide();            // Hide stimulus
+            setTimeout(function () {    // Wait briefly before presenting response options
+                $('#trialOptions').show();      // Present response options
+                stimPresenting = false;         // Permit responses
+            }, 100);
+        }, presentationTime);
+    }, 1000);
 }
 
-function loadTrialData(trialNum) {
-  trial = trials[trialNum];
+function loadTrialData() {
+    trial = trials[curTrial];
 
-  // Set the image
-  $('#trialImage').attr("src", trial["imageData"].src);
+    // Set the image
+    $('#trialImage').attr("src", trial["imageData"].src);
 
-  // Randomly assign the response options to true/foil answers
-  let trueOrder = Math.random() < 0.5;
-  responseOptionValues = trueOrder ? [1, 0] : [0, 1];
-  var trueText = trueOrder ? $('#option1') : $('#option2');
-  var foilText = trueOrder ? $('#option2') : $('#option1');
-  trueText.html(trial["trueCat"]);
-  foilText.html(trial["foilCat"]);
-
-  // Set the results that need to be saved
-  if (!training) {
-    displayedImages.push(trial["image"]);
-    types.push(trial["type"]);
-    trueCats.push(trial["trueCat"]);
-    foilCats.push(trial["foilCat"]);
-  }
+    // Randomly assign the response options to true/foil answers
+    let trueOrder = Math.random() < 0.5;
+    responseOptionValues = trueOrder ? [1, 0] : [0, 1];
+    var trueText = trueOrder ? $('#option1') : $('#option2');
+    var foilText = trueOrder ? $('#option2') : $('#option1');
+    trueText.html(trial["trueCat"]);
+    foilText.html(trial["foilCat"]);
 }
 
 function finishedTraining() {
-  canProceed = false;
-  $('#trainEndWarning').show();
-  training = false;
-  $('#proceedExperiment').click(function () {
-    canProceed = true;
-    $('#trainEndWarning').hide();
-  });
+    canProceed = false;
+    $('#trainEndWarning').show();
+    $('#proceedExperiment').click(function () {
+        canProceed = true;
+        $('#trainEndWarning').hide();
+        $('#nextTrialMessage').show();
+    });
+}
+
+function doneExperiment() {
+    exportData();
+    $("#trial").hide();
+    $(document).unbind("keydown.responded");
+    $(document).unbind("keydown.nextTrial");
+    $("#submitButton").show();
 }
 
 function startExperiment() {
-  $('#startExperiment').hide();
-  $('#instructionsContainer').hide();
-  $('#trial').show();
-  trialBegin(0);
-  trialStartTime = new Date();
+    $('#startExperiment').hide();
+    $('#instructionsContainer').hide();
+    $('#trial').show();
+    trialBegin(0);
+    trialStartTime = new Date();
+
+
+    // Click events
+
+    // User has selected a response (pressed a key)
+    $(document).bind("keydown.responded", function (event) {
+        // Check if the key corresponds to a valid response
+        if ((event.which != 68 && event.which != 75) || stimPresenting) {
+            return;
+        }
+
+        // If this is the last training image, give a warning that must be acknowledged before continuing
+        if (curTrial === nTraining - 1 && curResponse === null) {
+            finishedTraining();
+        }
+
+        // Allow user to continue to the next trial
+        if (canProceed) {
+            $('#nextTrialMessage').show();
+        }
+
+        // Register which response was made
+        if (event.which == 68) {
+            curResponse = responseOptionValues[0];
+            $('#option1box').css("background-color", "lightgrey");
+            $('#option2box').css("background-color", "white");
+        } else {
+            curResponse = responseOptionValues[1];
+            $('#option2box').css("background-color", "lightgrey");
+            $('#option1box').css("background-color", "white");
+        }
+    });
+
+    // User wishes to continue to the next trial (pressed the "Space" key)
+    $(document).bind("keydown.nextTrial", function (event) {
+        // Check if they pressed the space bar and that they've responded
+        // (and that they've acknowledged being done training)
+        if (event.which == 32 && curResponse != null && canProceed) {
+            $('#nextTrialMessage').hide();
+            $('#trialOptions').hide();
+            $('#option1box').css("background-color", "white");
+            $('#option2box').css("background-color", "white");
+            if (curTrial === nTraining - 1) {                   // If training has ended
+                $("#sessionMode").html("Experiment segment")
+            }
+            trialDone();
+        }
+    });
 }
 
 function exportData() {
-  $('#displayedImages').val(displayedImages.join());
-  $('#types').val(types.join());
-  $('#trueCats').val(trueCats.join());
-  $('#foilCats').val(foilCats.join());
-  $('#trialResponses').val(trialResponses.join());
-  $('#reactionTimes').val(reactionTimes.join());
+    $('#displayedImages').val(displayedImages.join());
+    $('#types').val(types.join());
+    $('#trueCats').val(trueCats.join());
+    $('#foilCats').val(foilCats.join());
+    $('#responses').val(responses.join());
+    $('#reactionTimes').val(reactionTimes.join());
 }
 
 /* Setup/preloading code */
 function getTrials(callback) {
-  $.getJSON(rootPath + "assets/stimuli.json", function(data) {
-    var catchStimuli = data["catchStimuli"];
-    var trainStimuli = data["trainStimuli"];
-    var stimuli = shuffle(data["stimuli"]);
-    var foilCats = data["foilCategories"];
+    $.getJSON(rootPath + "assets/stimuli.json", function (data) {
+        var catchStimuli = data["catchStimuli"];
+        var trainStimuli = data["trainStimuli"];
+        var stimuli = shuffle(data["stimuli"]);
 
-    nTraining = trainStimuli.length;
-    if (nTrials == null) {
-      nTrials = stimuli.length;
-    }
+        nTraining = trainStimuli.length;
+        if (nTrials == null) {
+            nTrials = stimuli.length;
+        }
 
-    for (var iStim = 0; iStim < catchStimuli.length; iStim++) {
-      var stimulus = catchStimuli[iStim];
-      stimulus["type"] = "catch";
-      stimulus["foilCat"] = sample(foilCats);
-    }
-    shuffle(catchStimuli);
+        for (var iStim = 0; iStim < catchStimuli.length; iStim++) {
+            var stimulus = catchStimuli[iStim];
+            stimulus["type"] = "catch";
+        }
+        shuffle(catchStimuli);
 
-    for (var iStim = 0; iStim < nTraining; iStim++) {
-      var stimulus = trainStimuli[iStim];
-      stimulus["type"] = "training";
-      stimulus["foilCat"] = sample(foilCats);
-    }
-    shuffle(trainStimuli);
+        for (var iStim = 0; iStim < nTraining; iStim++) {
+            var stimulus = trainStimuli[iStim];
+            stimulus["type"] = "training";
+        }
+        shuffle(trainStimuli);
 
-    let types = ["disrupted", "random", "original"];
-    for (var iStim = 0, iType = 0; iStim < nTrials; iStim++, iType++) {
-      var stimulus = stimuli[iStim];
-      stimulus["type"] = types[iType % types.length];
-      stimulus["foilCat"] = sample(foilCats);
-    }
-    stimuli = shuffle(stimuli);
+        let types = ["disrupted", "random", "original"];
+        for (var iStim = 0, iType = 0; iStim < nTrials; iStim++, iType++) {
+            var stimulus = stimuli[iStim];
+            stimulus["type"] = types[iType % types.length];
+        }
+        stimuli = shuffle(stimuli);
 
-    var trialsWithCatches = [];
-    for (var iStim = 0, iCatch = 0; iStim < stimuli.length; iStim++) {
-      trialsWithCatches.push(stimuli[iStim]);
-      if (iStim % catchFreq == catchFreq - 1) {
-        trialsWithCatches.push(catchStimuli[iCatch]);
-        iCatch++;
-      }
-    }
+        var trialsWithCatches = [];
+        for (var iStim = 0, iCatch = 0; iStim < stimuli.length; iStim++) {
+            trialsWithCatches.push(stimuli[iStim]);
+            if (iStim % catchFreq == catchFreq - 1) {
+                trialsWithCatches.push(catchStimuli[iCatch]);
+                iCatch++;
+            }
+        }
 
-    trials = trainStimuli.concat(trialsWithCatches);
-    nTrials = trials.length;
+        trials = trainStimuli.concat(trialsWithCatches);
+        nTrials = trials.length;
 
-    callback();
-  });
+        callback();
+    });
 }
 
 var imgCounter = 0;
 
 function preloadStimuli(callback) {
-  for (var i = 0; i < trials.length; i++) {
-    preloadImg(trials[i])
-  }
-  waitForStimuliToPreload(callback);
-  console.log('Image preloading complete.');
+    for (var i = 0; i < trials.length; i++) {
+        preloadImg(trials[i])
+    }
+    waitForStimuliToPreload(callback);
+    console.log('Image preloading complete.');
 }
 
 function preloadImg(trial) {
-  let imagePath = rootPath + "images/" + trial["type"] + "/" + trial["image"];
-  loadImage(imagePath).then((img) => {
-      console.log("Preloading:", img);
-      trial['imageData'] = img;
-      imgCounter++;
-      console.log('Image preloading progress: ' + Math.round(100 * (imgCounter / trials.length)) + '%');
-  });
+    let imagePath = rootPath + "images/" + trial["type"] + "/" + trial["image"];
+    loadImage(imagePath).then((img) => {
+        console.log("Preloading:", img);
+        trial['imageData'] = img;
+        imgCounter++;
+        console.log('Image preloading progress: ' + Math.round(100 * (imgCounter / trials.length)) + '%');
+    });
 }
 
 function loadImage(src) {
-    return new Promise((resolve, reject)=> {
+    return new Promise((resolve, reject) => {
         var img = new Image();
-        img.onload = ()=> resolve(img);
+        img.onload = () => resolve(img);
         img.src = src;
     });
 }
 
 function waitForStimuliToPreload(callback) {
-  if (imgCounter < trials.length) {
-      setTimeout(function() {waitForStimuliToPreload(callback)}, 24);
-  } else {
-      // load trial
-      callback();
-  }
+    if (imgCounter < trials.length) {
+        setTimeout(function () {
+            waitForStimuliToPreload(callback)
+        }, 24);
+    } else {
+        // load trial
+        callback();
+    }
 }
 
-$(document).ready(function() {
-  $('#submitButton').hide();
-  getTrials(function() {
-    preloadStimuli(function(){
-      $('#consent').click(function(){
-          $('#startExperiment').click(function(){
-              startExperiment();
-          });
-      });
-  });
-  });
+$(document).ready(function () {
+    $('#submitButton').hide();
+    getTrials(function () {
+        preloadStimuli(function () {
+            $('#consent').click(function () {
+                $('#startExperiment').click(function () {
+                    startExperiment();
+                });
+            });
+        });
+    });
 });
 
 /* Utility functions */
-function shuffle(o){
-  for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-  return o;
+function shuffle(o) {
+    for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x) ;
+    return o;
 }
 
 function sample(o) {
-  return o[Math.floor(Math.random() * o.length)];
+    return o[Math.floor(Math.random() * o.length)];
 }

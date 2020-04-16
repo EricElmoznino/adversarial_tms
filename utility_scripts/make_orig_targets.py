e27957d993eec108b9173142f19520c33634f8c2
EricElmoznino <eric.elmoznino@gmail.com>
2020-04-16 11:53:21 -0400

    Minor changes to bold5000 training

--- utility_scripts/make_orig_targets.py
+++ utility_scripts/make_orig_targets.py
@@ -10,11 +10,11 @@ resolution = 256
 
 for feat_name in feature_names:
     for roi in rois:
-        encoder = torch.load('saved_models/study=bold5000_featextractor=alexnet_featname={}_rois={}.pth'.format(feat_name, roi),
+        encoder = torch.load('saved_models/study=obj2vec_featextractor=alexnet_featname={}_rois={}.pth'.format(feat_name, roi),
                             map_location=lambda storage, loc: storage)
         for dataset in datasets:
             data_dir = os.path.join(dir, dataset)
-            target_dir = os.path.join(dir, 'targets_bold5000', '{}_roi={}_feat={}'.format(dataset, roi, feat_name))
+            target_dir = os.path.join(dir, 'targets', '{}_roi={}_feat={}'.format(dataset, roi, feat_name))
             os.mkdir(target_dir)
             image_names = os.listdir(data_dir)
             images_names = [img for img in image_names if img != '.DS_Store']

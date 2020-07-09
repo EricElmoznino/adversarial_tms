2097b2d58fc8263654cb099338bc0187bcb373fa
EricElmoznino <eric.elmoznino@gmail.com>
2020-07-08 23:46:52 -0400

    Fixed minor typo

--- bold5000/fit_pca_model.py
+++ bold5000/fit_pca_model.py
@@ -56,5 +56,5 @@ if __name__ == '__main__':
 
     encoder = PCAEncoder(feat_extractor, pcs=pca.components_, mean=pca.mean_)
     encoder.eval()
-    run_name = utils.get_run_name('bold5000', args.feature_extractor, args.feature_name, 'pca')
+    run_name = utils.get_run_name('bold5000', args.feature_extractor, args.feature_name, 'PCA')
     torch.save(encoder, os.path.join('saved_models', run_name + '.pth'))
